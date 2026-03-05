14b4011f124f39dc32eab1e2e4d861f45e5afde3
adrita06 <adritaahsan06@gmail.com>
2026-03-05 10:11:51 +0600

    update

--- kern/lib/debug.h
+++ kern/lib/debug.h
@@ -6,56 +6,71 @@
 #include "stdarg.h"
 
 #ifdef DEBUG_MSG
-#define KERN_DEBUG(...)                                \
-    do {                                               \
-        debug_normal(__FILE__, __LINE__, __VA_ARGS__); \
-    } while (0)
 
-#define KERN_WARN(...)                               \
-    do {                                             \
-        debug_warn(__FILE__, __LINE__, __VA_ARGS__); \
-    } while (0)
+#define KERN_DEBUG(...) do {					\
+		debug_normal(__FILE__, __LINE__, __VA_ARGS__);	\
+	} while (0)
 
-#define KERN_PANIC(...)                               \
-    do {                                              \
-        debug_panic(__FILE__, __LINE__, __VA_ARGS__); \
-    } while (0)
+#define KERN_WARN(...) do {					\
+		debug_warn(__FILE__, __LINE__, __VA_ARGS__);	\
+	} while (0)
 
-#define KERN_ASSERT(x)                                       \
-    do {                                                     \
-        if (!(x))                                            \
-            KERN_PANIC("Kernel assertion failed: %s\n", #x); \
-    } while (0)
-#else   /* !DEBUG_MSG */
-#define KERN_DEBUG(...) do {} while (0)
+#define KERN_PANIC(...) do {					\
+		debug_panic(__FILE__, __LINE__, __VA_ARGS__);	\
+	} while (0)
 
-#define KERN_WARN(...) do {} while (0)
+#define KERN_ASSERT(x) do {						\
+		if (!(x))						\
+			KERN_PANIC("Kernel assertion failed: %s\n", #x); \
+	} while(0)
 
-#define KERN_PANIC(...) do {} while (0)
+#else /* !DEBUG_MSG */
 
-#define KERN_ASSERT(c) do {} while (0)
-#endif  /* DEBUG_MSG */
+#define KERN_DEBUG(...) do {			\
+	} while (0)
 
-#define KERN_INFO(fmt, ...)             \
-    do {                                \
-        debug_info(fmt, ##__VA_ARGS__); \
-    } while (0)
+#define KERN_WARN(...) do {			\
+	} while (0)
 
-void vprintfmt(void (*putch)(int, void *), void *putdat, const char *fmt,
-               va_list ap);
+#define KERN_PANIC(...) do {			\
+	} while (0)
 
-void debug_info(const char *fmt, ...);
+#define KERN_ASSERT(c) do {			\
+	} while (0)
 
+#endif /* DEBUG_MSG */
+
+#define KERN_INFO(fmt, ...) do {		\
+		debug_info(fmt, ##__VA_ARGS__);	\
+	} while (0)
+
+void vprintfmt(void (*putch)(int, void *), void *, const char *, va_list);
+
+void debug_info(const char *, ...);
 #ifdef DEBUG_MSG
-int dprintf(const char *fmt, ...);
 
-void debug_normal(const char *file, int line, const char *fmt, ...);
-void debug_warn(const char *file, int line, const char *fmt, ...);
-void debug_panic(const char *file, int line, const char *fmt, ...);
-#else   /* DEBUG_MSG */
-#define dprintf(...) do {} while (0)
-#endif  /* !DEBUG_MSG */
+int dprintf(const char *, ...);
 
-#endif  /* _KERN_ */
 
-#endif  /* !_KERN_LIB_DEBUG_H_ */
+void debug_normal(const char *, int, const char *, ...);
+void debug_warn(const char*, int, const char*, ...);
+void debug_panic(const char*, int, const char*, ...);
+
+
+void debug_init(void);
+void debug_lock(void);
+void debug_unlock(void);
+void serial_lock(void);
+void serial_unlock(void);
+void assert_serial_locked(void);
+
+#else
+
+#define dprintf(...) do {			\
+	} while (0)
+
+#endif
+
+#endif /* _KERN_ */
+
+#endif /* !_KERN_LIB_DEBUG_H_ */
--- kern/lib/monitor.h
+++ kern/lib/monitor.h
@@ -1,5 +1,5 @@
-#ifndef _KERN_LIB_MONITOR_H_
-#define _KERN_LIB_MONITOR_H_
+#ifndef _KERN_LIB_MONITOR_H
+#define _KERN_LIB_MONITOR_H
 
 #ifdef _KERN_
 
@@ -11,11 +11,11 @@ struct Trapframe;
 void monitor(struct Trapframe *tf);
 
 // Functions implementing monitor commands.
+int mon_start_user(int argc, char **argv, struct Trapframe *tf);
 int mon_help(int argc, char **argv, struct Trapframe *tf);
 int mon_kerninfo(int argc, char **argv, struct Trapframe *tf);
 int mon_backtrace(int argc, char **argv, struct Trapframe *tf);
-int mon_start_user(int argc, char **argv, struct Trapframe *tf);
 
-#endif  /* _KERN_ */
+#endif /* _KERN_ */
 
-#endif  /* !_KERN_LIB_MONITOR_H_ */
+#endif	// !_KERN_LIB_MONITOR_H
